60ef9382d732a02a9f1c9b3a087cb3b63585c5eb
Adrien <adrien.biz@gmail.com>
2022-06-18 15:00:36 +0400

    Create structure for FizzBuzz problem
    Prompt user for a number. Display FizzBuzz sequence in console.

---
+++ script.js
@@ -0,0 +1,13 @@
+const input = parseInt(prompt("Please enter a number"))
+
+for (let i = 1; i <= input; i++) {
+    if (i % 15 === 0){
+        console.log('FizzBuzz')
+    } else if (i % 3 === 0) {
+        console.log('Fizz');
+    } else if (i % 5 === 0) {
+        console.log('Buzz');
+    } else {
+        console.log(i);
+    }
+}
